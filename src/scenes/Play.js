8a874bc8cfc236d787249f86797fe5801fa085e1
Emil Sabev <esabev@ucsc.edu>
2024-03-08 04:12:25 -0800

    added animations for 360-side-flip and for wind and functionality for them

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,105 +1,157 @@
-class Play extends Phaser.Scene 
+/*
+used chatGPT to help with setting up airborne trick animation: https://chat.openai.com/share/c1fc02b7-baef-4834-9dc6-f90363d22249
+*/
+
+class Play extends Phaser.Scene
 {
     constructor()
     {
         super('playScene')
-        /*
-        this.timeAlive = 0
-        this.isJumping = false
-        this.ground
-        this.ninja
-        this.mine
-        this.speedIncrease = 20000
-        */
+        this.keysPressed = ''
+        this.lastKeyPressTime = 0
     }
 
-    preload()
+    preload() //load images/atlases
     {
         this.load.image("play_background", "./assets/play_background.png")
+        this.load.atlas("360_side_flip_anims", "./assets/bike_360_side_flip.png", "./assets/bike_360_side_flip.json")
+        this.load.image("trick_wind", "./assets/trick_wind.png")
     }
 
-    create() 
+    create() //add background, biker, biker animations, tricks, wind, jumping, and collision
     {
-        const tricks = 
+        //make background visible
+        this.backgroundImage = this.add.tileSprite(0, 0, 800, 600, "play_background").setOrigin(0, 0)
+
+        //add biker and set to stationary frame
+        this.biker = this.physics.add.sprite(150, 470, 'bike_ramp_anims')
+        this.biker.setFrame('bike_ramp_1.png')
+
+        //modify biker properties
+        this.biker.setVelocity(0)
+        this.biker.body.setSize(50, 50)
+
+        //add invisibile ground wall with collider
+        const wall = this.physics.add.staticSprite(0, 500)
+        wall.setSize(800, 1)
+        this.physics.add.collider(this.biker, wall)
+
+        //set jumping properties and animations
+        setInterval(() => 
+        {
+            this.biker.setVelocityY(-380)
+            this.biker.anims.play('rampAnims')
+
+            setTimeout(() => 
+            {
+                this.biker.setVelocityY(0)
+                //this.biker.setFrame('bike_ramp_1.png')
+            }, 1800)
+        }, 2500)
+
+        this.elapsedTime = 0;
+
+        //trick index
+        this.tricks = 
         {
             "360_side_flip": ["D", "W", "A", "S"],
             "head_stand": ["F, R, E, T"],
             "sexy_move": ["Q", "Q", "Q", "Q"]
         }
 
-        this.backgroundImage = this.add.tileSprite(0, 0, 800, 600, "play_background").setOrigin(0, 0)
+        //360-side-flip animations
+        this.anims.create
+        ({
+            key: '360sideflipAnims',
+            frames: this.anims.generateFrameNames('360_side_flip_anims', {prefix: "360_side_flip_", start: 1, end: 9, suffix: ".png"}),
+            frameRate: 7
+        })
 
+        //add trick wind sprite
+        this.trickWind = this.add.sprite(0, 0, 'trick_wind').setOrigin(0, 0)
+        this.trickWind.setVisible(false)
 
-        /*
-        //add background image to scene
-        this.backgroundImage = this.add.tileSprite(0, 0, 800, 600, 'PlayBackground').setOrigin(0, 0)
-        this.timeAlive = 0
+        //check for user keyboard input
+        this.input.keyboard.on('keydown', this.handleKeyDown, this)
 
-        //set up ninja sprite
-        this.ninja = this.physics.add.sprite(100, 355, 'runAnims').setOrigin(0, 0)
-        this.ninja.setCollideWorldBounds(true)
-        this.ninja.setOffset(this.ninja.width / 2, this.ninja.height / 2 + 30)
-        this.ninja.anims.play('runAnims')
-        this.ninja.on('animationcomplete', function(animation, frame)
-        {
-            if (animation.key === 'jumpAnims')
-            {
-                this.ninja.anims.play('runAnims')
-                this.isJumping = false
-            }
-        }, this)
+    }
 
-        this.mine = this.physics.add.sprite(Phaser.Math.Between(800, 1600), 407, 'mineAnims').setOrigin(0, 0)
-        this.mine.setScale(1.5)
-        this.mine.anims.play('mineAnims')
-        this.mine.body.allowGravity = false
-        this.mine.body.immovable = true
-        this.mine.setCollideWorldBounds(false)
-        this.mine.setOffset(this.mine.width / 2 - 16, this.mine.height / 2 - 20)
-        this.mine.setSize(30, 12)
-        this.mine.setOrigin(0, 0)
-        this.mineSpeed = -700
-        this.mine.setVelocityX(this.mineSpeed)
+    update() //track biker jumping state, trick state, wind state, and time elapsed
+    {
         
-        //set up ground sprite
-        this.ground = this.physics.add.sprite(400, 745, "groundCollision")
-        this.ground.body.allowGravity = false
-        this.ground.body.immovable = true
+        this.elapsedTime = this.time.now
 
-        //add collision and physics
-        this.physics.add.collider(this.ninja, this.ground)
-        this.physics.add.collider(this.ninja, this.mine, this.handleMineCollision, null, this);
-        this.physics.world.gravity.y = 5000
+        //move trick wind by -3.5
+        this.trickWind.x -= 3.5
 
-        //add space for jump
-        this.jumpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        //wrap screen if necessary
+        if (this.trickWind.x > this.game.config.width) 
+        {
+            this.trickWind.x = 0
+        }
 
-        //display time alive clock
-        this.displayClock = this.add.text(235, 50, 'Time Elapsed: 0', {fontSize: '40px', fontFamily: 'Monospace'})
-        this.clock = this.time.addEvent
-        ({
-            delay: 1000,
-            callback: this.updateClock,
-            callbackScope: this,
-            loop: true
-        })
+        //3 second stationary background to start the game
+        if (this.elapsedTime > 3050) 
+        {
+            this.backgroundImage.tilePositionX += 2.5 + (this.elapsedTime / 100000 - 0.005)
+        }
 
-        this.time.addEvent
-        ({
-            delay: this.speedIncrease,
-            callback: this.increaseMineSpeed,
-            callbackScope: this,
-            loop: true
-        })
+        //check for user keyboard input for tricking
+        if (this.biker.y < 470)
+        {
+            if (this.keySequence === 'FRET' && this.elapsedTime - this.lastKeyPressTime <= 500)
+            {
+                this.trickWind.x = 0
+                this.trickWind.y = 0
+                this.handleTrickWind()
 
-        this.mineSpeedText = this.add.text(75, 225, '      Mine speed will start \n   increasing every 20 seconds', {fontSize: '40px', fontFamily: 'Monospace', visible: false})
-        this.mineSpeedText.setVisible(false)
+                this.biker.anims.play('360sideflipAnims')
+                this.keySequence = ''
+                this.lastKeyPressTime = 0
+            }
+        }
+        
+        //set frame to stationary if biker is on the floor (470y)
+        /*
+        if (this.biker.y == 470)
+        {
+            this.biker.setFrame('bike_ramp_1.png')
+        }
         */
     }
 
-    update() //check collisions, game over, movement/animation 
+    handleKeyDown(event) //function that handles the key combinations pressed for biker tricking. inputs are calculated in 0 - 500 ms intervals
     {
-        this.backgroundImage.tilePositionX += 3
-    }
+        const key = event.key.toUpperCase()
+        const currentTime = this.time.now
+             
+        if (currentTime - this.lastKeyPressTime > 500)
+        {
+            this.keySequence = key
+        }
 
+        else
+        {
+            this.keySequence += key
+        }
+
+        this.lastKeyPressTime = currentTime        
+    }
+    
+    handleTrickWind() //set trick wind to visible if biker is in tricking state
+    {
+        this.trickWind.setVisible(true);
+        this.trickWindTimer = this.time.addEvent
+        ({
+            delay: 1000,
+            callback: this.hideTrickWind, 
+            callbackScope: this
+        })
+    }
+    
+    hideTrickWind() //set trick wind to not visible after handleTrickWind() is called
+    {
+        this.trickWind.setVisible(false)
+    }
+    
 }
