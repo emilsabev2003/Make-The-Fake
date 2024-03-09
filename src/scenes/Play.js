2f1c2527cac0ed4b315997dda2fe7433b4dcd502
Emil Sabev <esabev@ucsc.edu>
2024-03-09 06:42:09 -0800

    added  sexymove and headstand animations and implemented them

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,6 +16,8 @@ class Play extends Phaser.Scene
         this.load.image("play_background", "./assets/play_background.png")
         this.load.atlas("360_side_flip_anims", "./assets/bike_360_side_flip.png", "./assets/bike_360_side_flip.json")
         this.load.image("trick_wind", "./assets/trick_wind.png")
+        this.load.atlas("sexy_move_anims", "./assets/sexy_move_anims.png", "./assets/sexy_move_anims.json")
+        this.load.atlas("headstand_anims", "./assets/headstand_anims.png", "./assets/headstand_anims.json")
     }
 
     create() //add background, biker, biker animations, tricks, wind, jumping, and collision
@@ -26,6 +28,7 @@ class Play extends Phaser.Scene
         //add biker and set to stationary frame
         this.biker = this.physics.add.sprite(150, 470, 'bike_ramp_anims')
         this.biker.setFrame('bike_ramp_1.png')
+        
 
         //modify biker properties
         this.biker.setVelocity(0)
@@ -64,6 +67,20 @@ class Play extends Phaser.Scene
         ({
             key: '360sideflipAnims',
             frames: this.anims.generateFrameNames('360_side_flip_anims', {prefix: "360_side_flip_", start: 1, end: 9, suffix: ".png"}),
+            frameRate: 7
+        })
+
+        this.anims.create
+        ({
+            key: 'sexyMoveAnims',
+            frames: this.anims.generateFrameNames('sexy_move_anims', {prefix: "Sexy_move_", start: 1, end: 6, suffix: ".png"}),
+            frameRate: 7
+        })
+
+        this.anims.create
+        ({
+            key: 'headStandAnims',
+            frames: this.anims.generateFrameNames('headstand_anims', {prefix: "headstand_", start: 1, end: 7, suffix: ".png"}),
             frameRate: 7
         })
 
@@ -107,6 +124,28 @@ class Play extends Phaser.Scene
 
                 this.biker.anims.play('360sideflipAnims')
                 this.keySequence = ''
+                this.lastKeyPressTime = 0
+            }
+
+            if (this.keySequence == "QQQQ" && this.elapsedTime - this.lastKeyPressTime <= 500)
+            {
+                this.trickWind.x = 0
+                this.trickWind.y = 0
+                this.handleTrickWind()
+
+                this.biker.anims.play("sexyMoveAnims")
+                this.keySequence = ""
+                this.lastKeyPressTime = 0
+            }
+
+            if (this.keySequence == "FRET" && this.elapsedTime - this.lastKeyPressTime <= 500)
+            {
+                this.trickWind.x = 0
+                this.trickWind.y = 0
+                this.handleTrickWind()
+
+                this.biker.anims.play("headStandAnims")
+                this.keySequence = ""
                 this.lastKeyPressTime = 0
             }
         }
